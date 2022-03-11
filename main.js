af6f1840425e9459db57041f7a255b3d6d22dee1
Firdavs <firdavssherzodovic@gmail.com>
2022-03-11 17:40:31 +0400

    tugatolmadim

---
+++ main.js
@@ -0,0 +1,7 @@
+let h4  = document.querySelector('#desc')
+let h1  = document.querySelector('#text')
+let btn = document.querySelector('#btn')
+
+btn.onclick = (event) => (
+    prompt(1-raqamni_kirit)
+)
